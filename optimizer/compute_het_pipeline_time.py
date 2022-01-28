8e3cde5da60f82ad2c09b50e1d63360e0b6bc68d
Daiyaan Arfeen <marfeen@andrew.cmu.edu>
2022-01-28 13:43:40 -0500

    pipeline analysis

---
+++ optimizer/compute_het_pipeline_time.py
@@ -0,0 +1,18 @@
+import json
+
+A = json.load(open('A.json', 'r'))
+compute_times = json.load(open('compute_times.json', 'r'))
+for i in range(len(compute_times)):
+    for j in range(len(compute_times[0])):
+        compute_times[i][j] = compute_times[i][j][-1][0]
+parameter_sizes = json.load(open('parameter_sizes.json', 'r'))
+
+def compute_time(k, j, speed, m_prime=1, bandwidth=3125000000.0):
+    last_stage_time = compute_times[k+1][j]
+    last_stage_parameter_size = parameter_sizes[k+1][j]
+    last_stage_time = sum([last_stage_time,
+    ((4 * (m_prime - 1) *
+        last_stage_parameter_size) / (bandwidth * m_prime))])   
+    last_stage_time /= m_prime
+    last_stage_time /= speed
+    return last_stage_time
