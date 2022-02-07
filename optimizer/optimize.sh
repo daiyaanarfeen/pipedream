f21952dac44b2295ccdf167cfddc26e36816fa70
Daiyaan Arfeen <marfeen@andrew.cmu.edu>
2022-02-07 12:20:47 -0500

    gnmt subgraph analysis

---
+++ optimizer/optimize.sh
@@ -0,0 +1,1 @@
+python optimizer_graph_hierarchical.py -n 2 2 -b 16e9 3.125e9 -s 16e9 -f ../profiler/translation/profiles/gnmt/graph_body.txt --activation_compression_ratio 1 --straight_pipeline --use_memory_constraint -o gnmt_body
