562345666964b7c35b284de981606ab87cdf9b16
Daiyaan Arfeen <marfeen@andrew.cmu.edu>
2022-03-17 12:01:43 -0400

    heterogeneous pipeline analysis

--- optimizer/compute_het_pipeline_time.py
+++ optimizer/compute_het_pipeline_time.py
@@ -25,6 +25,12 @@ for i in range(args.num_gpus):
                     nodes.append((k, v))
     stage2nodes[i] = nodes
 
+predecessors = []
+for antichain_node in antichain_nodes:
+    antichain_node = antichain_node.antichain
+    predecessors.append(gr.all_predecessors(antichain_node))
+
+
 cur_end = len(A) - 2
 super_stage_splits = [cur_end+1]
 cur_machines_left = len(A[0][cur_end]) - 1
@@ -35,36 +41,45 @@ while True:
     super_stage_splits.insert(0, cur_end+1)
 super_stage_splits.insert(0, 0)
 
-sub_stage_splits = []
-for i in range(len(super_stage_splits)-1):
-    beg = super_stage_splits[i]
-    cur_end = super_stage_splits[i+1] - 1
-    cur_stage_splits = [cur_end+1]
-    cur_machines_left = len(compute_times[beg][cur_end]) - 1
-    while True:
-        if compute_times[beg][cur_end][cur_machines_left][1] is None:
-            break
-        cur_end, cur_machines_left = compute_times[beg][cur_end][cur_machines_left][1]
-        cur_stage_splits.insert(0, cur_end+1)
-    cur_stage_splits.insert(0, beg)
-    sub_stage_splits.append(cur_stage_splits)
-
-#super_stage_splits = [0, 3, 9, 94, 99]
-#sub_stage_splits = [[0, 1, 3], [3, 5, 9], [9, 84, 94], [94, 97, 99]]
-for i in range(len(super_stage_splits)-1):
-    print(compute_times[super_stage_splits[i]][super_stage_splits[i+1]-1][-1])
+def print_pipeline(super_stage_splits):
+    sub_stage_splits = []
+    for i in range(len(super_stage_splits)-1):
+        beg = super_stage_splits[i]
+        cur_end = super_stage_splits[i+1] - 1
+        cur_stage_splits = [cur_end+1]
+        cur_machines_left = len(compute_times[beg][cur_end]) - 1
+        while True:
+            if compute_times[beg][cur_end][cur_machines_left][1] is None:
+                break
+            cur_end, cur_machines_left = compute_times[beg][cur_end][cur_machines_left][1]
+            cur_stage_splits.insert(0, cur_end+1)
+        cur_stage_splits.insert(0, beg)
+        sub_stage_splits.append(cur_stage_splits)
     
-    for j in range(len(sub_stage_splits[i])-1):
-        print('\t', compute_times[sub_stage_splits[i][j]][sub_stage_splits[i][j+1]-1][0])
-        if j != len(sub_stage_splits[i]) - 2:
-            print('\t', '| %f'%(2.0 * activation_sizes[sub_stage_splits[i][j+1]-1] / args.bw[0]))
-            print('\t', 'v')
+    for i in range(len(super_stage_splits)-1):
+        print(compute_times[super_stage_splits[i]][super_stage_splits[i+1]-1][-1])
+        
+        for j in range(len(sub_stage_splits[i])-1):
+            print('\t', compute_times[sub_stage_splits[i][j]][sub_stage_splits[i][j+1]-1][0])
+            if j != len(sub_stage_splits[i]) - 2:
+                print('\t', '| %f'%(2.0 * activation_sizes[sub_stage_splits[i][j+1]-1] / args.bw[0]))
+                print('\t', 'v')
+    
+        if i != len(super_stage_splits) - 2:
+            print('| %f'%(2.0 * activation_sizes[super_stage_splits[i+1]-1] / args.bw[1]))
+            print('v')
 
-    if i != len(super_stage_splits) - 2:
-        print('| %f'%(2.0 * activation_sizes[super_stage_splits[i+1]-1] / args.bw[1]))
-        print('v')
+import numpy as np
+multiplier = np.array([1, 1, 1, 1])
+times = [max(np.array([compute_times[0][i][-1][0], compute_times[i+1][j][-1][0], compute_times[j+1][k][-1][0], compute_times[k+1][98][-1][0]]) / multiplier) \
+    for k in range(99) for j in range(k) for i in range(j) \
+    if compute_times[0][i][-1][0] is not None and compute_times[i+1][j][-1][0] is not None and compute_times[j+1][k][-1][0] is not None and compute_times[k+1][98][-1][0] is not None] 
+splits = [(0, i+1, j+1, k+1, 99) for k in range(99) for j in range(k) for i in range(j) \
+    if compute_times[0][i][-1][0] is not None and compute_times[i+1][j][-1][0] is not None and compute_times[j+1][k][-1][0] is not None and compute_times[k+1][98][-1][0] is not None]
 
-predecessors = []
-for antichain_node in antichain_nodes:
-    antichain_node = antichain_node.antichain
-    predecessors.append(gr.all_predecessors(antichain_node))
+multiplier = np.array([1, 1, 2])
+times = [max(np.array([compute_times[0][i][-1][0], compute_times[i+1][j][-1][0], compute_times[j+1][98][-1][0]]) / multiplier) \
+    for j in range(99) for i in range(j) \
+    if compute_times[0][i][-1][0] is not None and compute_times[i+1][j][-1][0] is not None and compute_times[j+1][98][-1][0] is not None] 
+splits = [(0, i+1, j+1, 99) for j in range(99) for i in range(j) \
+    if compute_times[0][i][-1][0] is not None and compute_times[i+1][j][-1][0] is not None and compute_times[j+1][98][-1][0] is not None]
